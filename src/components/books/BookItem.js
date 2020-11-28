5439b459535da9e377bf66b92ce557d04d0c0555
Opshanka <opibabeykoon@gmail.com>
2020-11-28 15:15:24 +0530

    Responsive card added

--- src/components/books/BookItem.js
+++ src/components/books/BookItem.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 const BookItem = ({ book }) => {
   return (
-    <div className='col s4'>
+    <div className='col s12 m6 l4'>
       <div className='card'>
         <div className='card-image waves-effect waves-block waves-light'>
-          <img className='activator' alt={book.title} src={book.book_image} />
+          <img
+            className='activator'
+            alt={book.title}
+            src={book.book_image}
+            style={{ margin: 'auto', width: 'auto', height: '400px' }}
+          />
         </div>
         <div className='card-content'>
           <span
-            className='card-title activator grey-text text-darken-4'
+            className='card-title activator grey-text text-darken-4 s11'
             style={{ fontSize: '12px' }}
           >
             {book.title}
-            <i className='material-icons right'>more_vert</i>
           </span>
         </div>
         <div className='card-reveal'>
